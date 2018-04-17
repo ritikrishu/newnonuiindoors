3afea43b07d8275adc4ef9d7a9e37a2caed9fdc3
Ritik Rishu <ritik.rishu@hotcocoasoftware.com>
2018-04-18 04:22:29 +0530

    new sdk

--- app/src/main/java/com/ritikrishu/newnonuiindoors/MainActivity.java
+++ app/src/main/java/com/ritikrishu/newnonuiindoors/MainActivity.java
@@ -2,10 +2,12 @@ package com.ritikrishu.newnonuiindoors;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.customlbs.library.Indoors;
@@ -17,6 +19,7 @@ import com.customlbs.library.callbacks.IndoorsServiceCallback;
 import com.customlbs.library.callbacks.LoadingBuildingStatus;
 import com.customlbs.library.model.Building;
 import com.customlbs.library.model.Zone;
+import com.customlbs.library.util.IndoorsCoordinateUtil;
 import com.customlbs.shared.Coordinate;
 
 import java.util.List;
@@ -24,6 +27,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity implements IndoorsServiceCallback, IndoorsLocationListener {
 
     private Indoors indoors;
+    public static final String TAG = "SGIND";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -66,47 +70,49 @@ public class MainActivity extends AppCompatActivity implements IndoorsServiceCal
 
     @Override
     public void loadingBuilding(LoadingBuildingStatus loadingBuildingStatus) {
-
+        Log.d(TAG, loadingBuildingStatus.getPhase() + " <- loading");
     }
 
     @Override
     public void buildingLoaded(Building building) {
-
+        Log.d(TAG, building.getName() + " <- building loaded");
     }
 
     @Override
     public void leftBuilding(Building building) {
-
+        Log.d(TAG, building.getName() + " <- building left");
     }
 
     @Override
     public void buildingReleased(Building building) {
-
+        Log.d(TAG, building.getName() + " <- building released");
     }
 
     @Override
     public void positionUpdated(Coordinate coordinate, int i) {
-
+        Location geoLocation = IndoorsCoordinateUtil.toGeoLocation(coordinate, mBuilding);
+        Log.d(TAG, coordinate.x + " -- " + coordinate.y + " <- positionUpdated x -- y");
+        Log.d(TAG, geoLocation.getLatitude() + " -- " + geoLocation.getLongitude() + " <- positionUpdated lat -- lng");
     }
 
     @Override
     public void orientationUpdated(float v) {
-
+        Log.d(TAG, v + " <- orientationUpdated");
     }
 
     @Override
     public void changedFloor(int i, String s) {
-
+        Log.d(TAG, s + " -- " + i + " <- changedFloor");
     }
 
     @Override
     public void enteredZones(List<Zone> list) {
-
+        Log.d(TAG, list.size() + " <- enteredZones list size");
     }
 
     @Override
     public void buildingLoadingCanceled() {
-
+        Log.d(TAG,"buildingLoadingCanceled");
     }
 
     @Override
@@ -120,6 +126,7 @@ public class MainActivity extends AppCompatActivity implements IndoorsServiceCal
 
     @Override
     public void onError(IndoorsException e) {
-
+        Log.d(TAG,"onError -> ");
+        e.printStackTrace();
     }
 }
